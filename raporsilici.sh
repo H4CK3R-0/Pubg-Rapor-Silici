3e59cdda81ad920406da8bdcd3bde70c36765b7e
x_H4CK3R_x <86078426+H4CK3R-0@users.noreply.github.com>
2021-12-14 22:10:46 +0300

    Update and rename .gitignore to raporsilici.sh

---
+++ raporsilici.sh
@@ -0,0 +1,32 @@
+mkdir @Raporsilici
+
+COUNT=1
+while
+do
+[ $COUNT ];
+sleep 1;
+sleep 1;
+date
+time
+
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/ProgramBinaryCache
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTracker/Saved/Config/Android/AntiCheat.ini
+rm -rf /storage/emulated/0/MidasOversea/GUID
+rm -rf /storage/emulated/0/tencent/Midas/Log/com.tencent.ig/20200201_1.txt
+rm -rf /storage/emulated/0/tencent/Midas/Log/com.tencent.ig/MidasLog.mmap
+
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTracker/Saved/Config/Android/AntiCheat.ini
+echo "Rapor Silici Başlatıldı!
+
+       Çalışıyor
+
+PUBG MOBİLE LOGS & REPORTS SİLİNİYOR"
+
+
+done
